72752d8155cbc0f07093b02d2fe17dbd3e9463a6
NixDevs <aniklakhany00@gmail.com>
2023-02-09 22:20:37 +0530

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,5 @@ import { defineConfig } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
     integrations: [],
-    site: 'https://astronaut.github.io',
-    base: '/'
+    site: 'https://nixdevs.github.io',
 });
